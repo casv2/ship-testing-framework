7eae9711c5dee07960c8a348312c7f4b5bb6f57f
casv2 <casv2@cam.ac.uk>
2019-09-21 10:57:27 +0100

    new zmethod run

--- tests/TiAl/Zmethod_TiAl/test.py
+++ tests/TiAl/Zmethod_TiAl/test.py
@@ -10,6 +10,6 @@ calculator = model.calculator
 at = read(os.path.join(os.path.abspath(os.path.dirname(__file__)), "bulk.xyz"))
 big_at = at * (6,6,6)
 
-Etot, Ekin, Epot, T, P = zmethod.Zmethod(calculator, big_at, nsteps=1000, dt=1, A=0.01, T=3500, R=50, save_config=50, name="TiAl")
+Etot, Ekin, Epot, T, P = zmethod.Zmethod(calculator, big_at, nsteps=1000, dt=1, A=0.02, T=3500, R=100, save_config=100, name="TiAl")
 
 properties = { 'E_tot' : Etot, 'E_kin' : Ekin, 'E_pot' : Epot, 'T' : T, 'P' : P }
